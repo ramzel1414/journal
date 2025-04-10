19772ebffa2fb1994a536ebbe65cdef90fbbe0c0
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-04-10 16:10:02 +0800

    chore: pretected route + update header + dedicated dashboard page

--- components/header.js
+++ components/header.js
@@ -1,7 +1,11 @@
+"use client";
+
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
+import { PenBox, FolderOpen, ChartNoAxesGantt } from 'lucide-react'
 
 const Header = () => {
   return (
@@ -17,12 +21,41 @@ const Header = () => {
         </Link>
         <div className="flex items-center gap-4">
           <SignedOut>
-                <SignInButton />
-                <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <Link href="/journal/write">
+              <Button variant="green" className="flex items-center gap-2">
+                <PenBox size={18}/>
+                <span className="hidden md:inline">Write a Journal</span>  
+              </Button>
+            </Link>
+            <SignInButton forceRedirectUrl='/dashboard'>
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <Link href="/dashboard/collections">
+              <Button variant="green" className="flex items-center gap-2">
+                <FolderOpen size={18}/>
+                <span className="hidden md:inline">Collections</span>  
+              </Button>
+            </Link>
+            <UserButton 
+              appearance={{
+                elements: {
+                  avatarBox: "w-16 h-16",
+                },
+              }}
+            >
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="Dashboard"
+                  labelIcon={<ChartNoAxesGantt size={15} />} 
+                  href="/dashboard"
+                />
+                <UserButton.Action label="manageAccount" />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
 
         </div>
       </nav>
