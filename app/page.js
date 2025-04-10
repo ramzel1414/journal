15ef29999e4a7272a11d9b69e24ae557ffe7da06
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-04-10 11:26:03 +0800

    install shadcn and test its button

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  const hello = 1
   return (
     <div>
-      THis is a JOurnal Aoo {hello}
+      THis is a JOurnal Aoo
+      <Button variant="link">Click me</Button>
     </div>
   );
 }
