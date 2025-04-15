ab10452dfdc725a9f7629fcdb3d0ce80f90606ca
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-04-15 15:23:38 +0800

    chore(journal/write/page): import the installed modules and crate a simple H! for display

--- app/(main)/journal/write/page.jsx
+++ app/(main)/journal/write/page.jsx
@@ -1,9 +1,30 @@
+"use client";
 
 import React from 'react'
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import dynamic from "next/dynamic";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import "react-quill-new/dist/quill.snow.css";
+
+
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 
 const JournalEntryPage = () => {
   return (
-    <div>JournalEntryPage</div>
+    <div className='py-8'>
+      <form action="">
+      <h1 className="text-5xl md:text-6xl gradient-title">
+          "What's on your mind?"
+        </h1>
+        {/* <h1 className="text-5xl md:text-6xl gradient-title">
+          {isEditMode ? "Edit Entry" : "What's on your mind?"}
+        </h1> */}
+      </form>
+
+    </div>
   )
 }
 
