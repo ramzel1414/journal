d9271c83c79cf308e8a03af9e5397c39a5131e6a
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-04-15 13:41:04 +0800

    chore(lib): store the user (clerk) to the neondb

--- components/header.jsx
+++ components/header.jsx
@@ -1,60 +1,54 @@
-"use client";
-
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
 import React from 'react'
+import Link from 'next/link'
 import { Button } from './ui/button'
 import { PenBox, FolderOpen, ChartNoAxesGantt } from 'lucide-react'
+import Image from 'next/image'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import UserMenu from "./user-menu";
+import { checkUser } from '@/lib/checkUser';
 
-const Header = () => {
+
+const Header = async () => {
+
+  // const user = await checkUser();
+
   return (
     <header className='container mx-auto'>
       <nav className='py-6 px-4 flex justify-between items-center'>
         <Link href={"/"}>
           <Image 
-            src={'/journal.jpg'} 
+            src={'/chelou.png'} 
             alt='Journal Logo' 
             width={60} 
             height={60} 
           />
         </Link>
         <div className="flex items-center gap-4">
-          <SignedOut>
-            <Link href="/journal/write">
-              <Button variant="green" className="flex items-center gap-2">
-                <PenBox size={18}/>
-                <span className="hidden md:inline">Write a Journal</span>  
+          <Link href="/journal/write">
+            <Button variant="green" className="flex items-center gap-2">
+              <PenBox size={18}/>
+              <span className="hidden md:inline">Write a Journal</span>  
+            </Button>
+          </Link>
+
+          <SignedIn>
+            <Link href="/dashboard/collections">
+              <Button variant="outline" className="flex items-center gap-2">
+                <FolderOpen size={18}/>
+                <span className="hidden md:inline">Collections</span>  
               </Button>
             </Link>
-            <SignInButton forceRedirectUrl='/dashboard'>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
               <Button variant="outline">Login</Button>
             </SignInButton>
           </SignedOut>
 
           <SignedIn>
-            <Link href="/dashboard/collections">
-              <Button variant="green" className="flex items-center gap-2">
-                <FolderOpen size={18}/>
-                <span className="hidden md:inline">Collections</span>  
-              </Button>
-            </Link>
-            <UserButton 
-              appearance={{
-                elements: {
-                  avatarBox: "w-16 h-16",
-                },
-              }}
-            >
-              <UserButton.MenuItems>
-                <UserButton.Link
-                  label="Dashboard"
-                  labelIcon={<ChartNoAxesGantt size={15} />} 
-                  href="/dashboard"
-                />
-                <UserButton.Action label="manageAccount" />
-              </UserButton.MenuItems>
-            </UserButton>
+
+            <UserMenu />
           </SignedIn>
 
         </div>
