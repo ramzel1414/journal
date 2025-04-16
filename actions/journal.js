54ad4d214289a3cda46f513bd230835a0c2109e9
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-04-16 11:32:18 +0800

    feat(lib/arcjet): limit rate entry

--- actions/journal.js
+++ actions/journal.js
@@ -5,7 +5,7 @@ import { db } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { getMoodById, MOODS } from "@/app/lib/moods";
 import { getPixabayImage } from "./public";
-// import aj from "@/lib/arcjet";
+import aj from "@/lib/arcjet";
 import { request } from "@arcjet/next";
 
 export async function createJournalEntry(data) {
@@ -18,28 +18,28 @@ export async function createJournalEntry(data) {
     // Get request data for ArcJet
     const req = await request();
 
-    // // Check rate limit
-    // const decision = await aj.protect(req, {
-    //   userId,
-    //   requested: 1, // Specify how many tokens to consume
-    // });
+    // Check rate limit
+    const decision = await aj.protect(req, {
+      userId,
+      requested: 1, // Specify how many tokens to consume
+    });
 
-    // if (decision.isDenied()) {
-    //   if (decision.reason.isRateLimit()) {
-    //     const { remaining, reset } = decision.reason;
-    //     console.error({
-    //       code: "RATE_LIMIT_EXCEEDED",
-    //       details: {
-    //         remaining,
-    //         resetInSeconds: reset,
-    //       },
-    //     });
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        const { remaining, reset } = decision.reason;
+        console.error({
+          code: "RATE_LIMIT_EXCEEDED",
+          details: {
+            remaining,
+            resetInSeconds: reset,
+          },
+        });
 
-    //     throw new Error("Too many requests. Please try again later.");
-    //   }
+        throw new Error("Too many requests. Please try again later.");
+      }
 
-    //   throw new Error("Request blocked");
-    // }
+      throw new Error("Request blocked");
+    }
 
     const user = await db.user.findUnique({
       where: { clerkUserId: userId },
