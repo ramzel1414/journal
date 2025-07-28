d36eb03d76fdd0f2d7caf26fd444b0381ce07453
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-07-29 00:46:23 +0800

    update: how about this, deploy na pls

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -1,11 +1,11 @@
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 // import { Email } from './email';
 
-const resend = new Resend('re_123456789');
+// const resend = new Resend('re_123456789');
 
-await resend.emails.send({
-  from: 'you@example.com',
-  to: 'user@gmail.com',
-  subject: 'hello world',
-  react: <Email url="https://example.com" />,
-});
+// await resend.emails.send({
+//   from: 'you@example.com',
+//   to: 'user@gmail.com',
+//   subject: 'hello world',
+//   react: <Email url="https://example.com" />,
+// });
